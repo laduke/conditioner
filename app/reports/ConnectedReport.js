cec1218c9106efa9ad0df5c57a2d9705ebd8a5a6
Travis LaDuke <travisladuke@gmail.com>
2016-10-27 14:26:58 -0700

    Place tides on the graph element.

--- app/reports/ConnectedReport.js
+++ app/reports/ConnectedReport.js
@@ -1,8 +1,15 @@
 import { connect } from 'react-redux';
 import h from 'react-hyperscript';
 import R from 'ramda';
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, MarkSeries } from 'react-vis';
+import moment from 'moment';
 
-
+const renameKeys = R.curry((keysMap, obj) => {
+  return R.reduce((acc, key) => {
+    acc[keysMap[key] || key] = obj[key];
+    return acc;
+  }, {}, R.keys(obj));
+});
 
 const report = props => {
   const {reports} = props;
@@ -18,18 +25,52 @@ const report = props => {
   const tempMax = R.path(['Weather', 'temp_max', 0])(firstSpot);
   const tempMin = R.path(['Weather', 'temp_min', 0])(firstSpot);
 
-  console.log(firstSpot, surfRange);
+  const tidesPath = R.pathOr([], ['Tide', 'dataPoints']);
+  const tideProps = R.pickAll(['height', 'Rawtime']);
+
+
+  //Rawtime: "October 27, 2016 13:00:00"
+  //Localtime: "2016-10-27 12:00:00"
+  const momentToHour = R.curry(time =>  moment(time, 'MMM DD, YYYY HH:mm:ss').hour() );
+  const rawTimeToHour = R.evolve({Rawtime: momentToHour});
+
+
+  const tides = R.pipe(
+    tidesPath,
+    R.map(tideProps),
+    R.map(rawTimeToHour),
+    R.map(renameKeys({height: 'y', Rawtime: 'x'})),
+    R.reject(R.propEq('y', 0))
+  );
+
+  let tmp = tides(firstSpot);
+  console.log( tmp );
+
 
 
   return (
-    h('div', {}, [
-      h('h1', {}, 'Southern Ca'),
-      h('h2', {}, 'Spot: ' + name),
-      h('p', {},  'Condition: ' + condition),
-      h('p', {},  'Surf: ' + surfRange),
-      h('p', {},  'Weather: ' + tempMin + '-' + tempMax),
-      h('p', {},  'Water: ' + waterTempMin + '-' + waterTempMax)
+  h('div', {}, [
+    h('h1', {}, 'Southern Ca'),
+    h('h2', {}, 'Spot: ' + name),
+    h('p', {}, 'Condition: ' + condition),
+    h('p', {}, 'Surf: ' + surfRange),
+    h('p', {}, 'Weather: ' + tempMin + '-' + tempMax),
+    h('p', {}, 'Water: ' + waterTempMin + '-' + waterTempMax),
+
+    h(XYPlot, {
+      width: 480,
+      height: 240
+    }, [
+      h(HorizontalGridLines),
+      h(MarkSeries, {
+        data: tides(firstSpot)
+      },
+      ),
+      h(XAxis),
+      h(YAxis)
     ])
+
+  ])
   );
 };
 
