0e6a2faacf5872d727480133cb1734d56134983a
Travis LaDuke <travisladuke@gmail.com>
2016-10-26 23:18:29 -0700

    Parse the simpler conditions out of the report.
    These don't need to be placed on a graph.
    Still need wind, tide, and sunrise/sunset.

--- app/reports/ConnectedReport.js
+++ app/reports/ConnectedReport.js
@@ -1,19 +1,35 @@
 import { connect } from 'react-redux';
 import h from 'react-hyperscript';
+import R from 'ramda';
 
 
 
 const report = props => {
   const {reports} = props;
 
-  const first = Object.keys(reports)[0];
+  const firstKey = R.compose(R.head, R.keys)(reports);
+  const firstSpot = R.prop(firstKey)(reports);
 
-  if (first) {
-    name = reports[first].name;
-  }
+  const name = R.prop('name')(firstSpot);
+  const condition = R.path(['Analysis', 'generalCondition'])(firstSpot);
+  const waterTempMax = R.path(['WaterTemp', 'watertemp_max'])(firstSpot);
+  const waterTempMin = R.path(['WaterTemp', 'watertemp_min'])(firstSpot);
+  const surfRange = R.path(['Analysis', 'surfRange', 0])(firstSpot);
+  const tempMax = R.path(['Weather', 'temp_max', 0])(firstSpot);
+  const tempMin = R.path(['Weather', 'temp_min', 0])(firstSpot);
+
+  console.log(firstSpot, surfRange);
+
 
   return (
-  h('h2', {}, 'Spot: ' + name)
+    h('div', {}, [
+      h('h1', {}, 'Southern Ca'),
+      h('h2', {}, 'Spot: ' + name),
+      h('p', {},  'Condition: ' + condition),
+      h('p', {},  'Surf: ' + surfRange),
+      h('p', {},  'Weather: ' + tempMin + '-' + tempMax),
+      h('p', {},  'Water: ' + waterTempMin + '-' + waterTempMax)
+    ])
   );
 };
 
