79b57e3d26835bca377929bcf80bae092e0b8b60
Travis LaDuke <travisladuke@gmail.com>
2016-11-03 13:31:50 -0700

    API cache was making router test hang.

--- app/reports/router.test.js
+++ app/reports/router.test.js
@@ -5,6 +5,7 @@ var test = require('tape');
 
 test('GET /api/reports/:spotID should send JSON list', function (assert) {
   app.get('/api/reports/4233')
+    .set('x-apicache-bypass', 'true')
     .expect(200)
     .expect('Content-Type', 'application/json; charset=utf-8')
     .end((err, res) => {
