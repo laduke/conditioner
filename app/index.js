afd2d20fd515f9abd4195917b1d7c4a44cf392b8
Travis LaDuke <travisladuke@gmail.com>
2016-11-24 14:00:49 -0800

    Use gzip compression.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const compression = require('compression');
 
+app.use(compression());
 app.use(require('./site/router'));
 app.use('/api/reports', require('./reports/router'));
 app.use('/api/distance', require('./travel-time/router'));
